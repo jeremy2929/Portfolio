be49411125aca9f5c226533d5b460e27f2b3d2b4
Jeremy Ward <jeremy2929@twc.com>
2017-04-06 02:17:55 -0500

    Slideshow development for projects

--- js/projects.js
+++ js/projects.js
@@ -3,44 +3,6 @@ import { render } from 'react-dom'
 import {Link} from 'react-router'
 import ReactDom from 'react-dom';
 
-// <div>
-//   <section className="projectsArea">
-//     <article className="cubeArea">
-//       <div className="wrap"  >
-//         <div className="cubeOut">
-//           <div className="cube">
-//             <h1 className="projectTitle">Project</h1>
-//             <div className="front">
-//               <p className="textFront">Sudoku Solver</p>
-//               <img className="sudokuImageCube" src="styles/sudoku.jpg" onClick={this.sudoku}/>
-//             </div>
-//             <div className="back">
-//               <p className="textBack">Chat App</p>
-//               <img className="chatAppCube" src="styles/chat.jpg" onClick={this.chatApp}/>
-//             </div>
-//             <div className="top"></div>
-//             <div className="bottom"></div>
-//             <div className="left">
-//               <p className="textLeft">Spurs Blog</p>
-//               <img className="spursBlogImageCube" src="styles/SpursBlog.jpg" onClick={this.spursBlog}/>
-//             </div>
-//            <div className="right">
-//              <p className="textLeft">Tic-Tac-Toe</p>
-//              <img className="tic-tac-toeImageCube" src="styles/tic-tac-toe.jpg" onClick={this.tic_tac_toe}/>
-//            </div>
-//          </div>
-//        </div>
-//      </div>
-//     </article>
-//   </section>
-// </div>
-
-
-
-// export default React.createClass({
-// render() {
-//   return (
-
 
   // sudoku() {
   //   window.open("https://sudoku-puzzle-sa.herokuapp.com", '_blank')
@@ -75,7 +37,7 @@ function prevDivs() {
 
 function showDivs(n) {
   var i;
-  var x = document.getElementsByClassName("my_projects");
+  var x = document.getElementsByClassName("project_area");
   if (n > x.length) {slideIndex = 1}
   if (n < 1) {slideIndex = x.length}
   for (i = 0; i < x.length; i++) {
@@ -83,11 +45,14 @@ function showDivs(n) {
   }
   x[slideIndex-1].style.display = "inline-block";
 }
+//<h1 className="tictactoe_title">Tic Tac Toe</h1>
+
 export function Projects() {
   var element = (
   <div>
-    <div className="sudoku_area my_projects first_project">
-       <p className="sudoku_title">Sudoku Solver</p>
+    <div className="project_area first_project">
+      <section className="fade">
+       <h1 className="sudoku_title">Sudoku Puzzle Solver</h1>
          <div className="wrap"  >
            <div className="cube">
              <div className="top"></div>
@@ -106,138 +71,61 @@ export function Projects() {
              <div className="bottom"></div>
           </div>
         </div>
+        <aside className="sudoku_description">
+          <p className="sudoku_text">
+            This app allows you to enter a Sudoku puzzle and then click Solve button to have the app solve the puzzle.  It will display an error message if a bad puzzle has been entered.  To enter a puzzle, click on each box and type in the number.  The method used to solve is methodical brute force.
+          </p>
+        </aside>
         <article className="slide_button_area">
-          <button className="slide_button_left" onClick={prevDivs}>&#10094;</button>
-          <button className="slide_button_right" onClick={plusDivs}>&#10095;</button>
+          <button className="slide_button_left" onClick={prevDivs}>&#8678;PREV</button>
+          <button className="slide_button_right" onClick={plusDivs}>NEXT&#8680;</button>
         </article>
+      </section>
     </div>
-    <div className="tictactoe_area my_projects">
-       <p className="sudoku_title">Not Sudoku Solver</p>
-         <div className="wrap"  >
-           <div className="cube">
-             <div className="top"></div>
-             <div className="front">
-               <img className="sudokuImageCube" src="styles/sudoku_solved.jpg"/>
-             </div>
-             <div className="back">
-               <img className="sudokuImageCube" src="styles/sudoku_solved.jpg"/>
-             </div>
-              <div className="right">
-               <img className="sudokuImageCube" src="styles/sudoku_puzzle.jpg"/>
-             </div>
-             <div className="left">
-               <img className="sudokuImageCube" src="styles/sudoku_puzzle.jpg"/>
-             </div>
-             <div className="bottom"></div>
-          </div>
-        </div>
-        <article className="slide_button_area">
-          <button className="slide_button_left" onClick={prevDivs}>&#10094;</button>
-          <button className="slide_button_right" onClick={plusDivs}>&#10095;</button>
-        </article>
+    <div className="project_area">
+      <section className="fade">
+       <h1 className="dollartrak_title">DollarTrak Budget App</h1>
+       <img className="project_image" src="styles/DollarTrak.jpg" />
+       <aside className="dollartrak_description">
+         <p className="dollartrak_text">
+           Week 13: A Budget App using React- full of functionality, storing data externally into Firebase, which I plan to re-factor into a more organized version with improved styling.
+         </p>
+       </aside> <article className="slide_button_area">
+         <button className="slide_button_left" onClick={prevDivs}>&#8678;PREV</button>
+         <button className="slide_button_right" onClick={plusDivs}>NEXT&#8680;</button>
+       </article>
+     </section>
   </div>
-
+  <div className="project_area">
+    <section className="fade">
+    <h1 className="spurs_blog_title">Spurs Blog Team Project</h1>
+    <img className="project_image" src="styles/SpursBlog.jpg" />
+      <aside className="spurs_blog_description">
+        <p className="spurs_blog_text">
+          Week 6: A four member team project on a given school assignment - All about the Spurs blog site.
+        </p>
+      </aside>
+    <article className="slide_button_area">
+      <button className="slide_button_left" onClick={prevDivs}>&#8678;PREV</button>
+      <button className="slide_button_right" onClick={plusDivs}>NEXT&#8680;</button>
+    </article>
+  </section>
+  </div>
+  <div className="project_area">
+    <section className="fade">
+    <img className="tictactoe_image" src="styles/tic-tac-toe.jpg" />
+      <aside className="tictactoe_description">
+        <p className="tictactoe_text">
+        Week 4: A functional Tic-Tac-Toe game in vanilla javascript for which I developed the logic myself.
+        </p>
+      </aside>
+    <article className="slide_button_area">
+      <button className="slide_button_left" onClick={prevDivs}>&#8678;PREV</button>
+      <button className="slide_button_right" onClick={plusDivs}>NEXT&#8680;</button>
+    </article>
+  </section>
+  </div>
    </div>
     )
   return element
 }
-
-
-// })
-
-
-
-//
-// var slideIndex = 1;
-//
-// function plusDivs() {
-//   slideIndex++
-//   if (slideIndex > 7){
-//     slideIndex = 1
-//   }
-//   showDivs(slideIndex);
-// }
-//
-// function prevDivs() {
-//   slideIndex--
-//   if (slideIndex > 7){
-//     slideIndex = 1
-//   }
-//   showDivs(slideIndex);
-// }
-//
-// function showDivs(n) {
-//   var i;
-//   var x = document.getElementsByClassName("mySlides");
-//   if (n > x.length) {slideIndex = 1}
-//   if (n < 1) {slideIndex = x.length}
-//   for (i = 0; i < x.length; i++) {
-//      x[i].style.display = "none";
-//   }
-//   x[slideIndex-1].style.display = "block";
-// }
-//
-// export function Photos() {
-//    var element = (
-//   <div>
-//     <div className="">
-//       <h1 className="theIronYardTitle">My experience at  </h1>
-//             <a className="theIronYard" href="https://www.theironyard.com/">The Iron Yard:
-//             </a>
-//     </div>
-//     <div className="">
-//       <div className="mySlides first_photo fade" id="image">
-//         <img className="slide_image" src="/styles/TIY.jpg" />
-//         <div className="caption1">
-//           Trolltunga, Norway
-//         </div>
-//       </div>
-//         <div className="mySlides fade" id="image">
-//         <img className="slide_image" src="/styles/cert.png" />
-//         <div className="caption1">
-//           Trolltunga, Norway
-//         </div>
-//       </div>
-//     <div className="mySlides fade" id="image">
-//       <img className="slide_image" src="styles/1.jpg" />
-//       <div className="caption1">
-//         Trolltunga, Norway
-//       </div>
-//     </div>
-//
-//     <div className="mySlides fade" id="image">
-//       <img className="slide_image" src="styles/2.jpg" />
-//       <div className="caption1">
-//         Northern Lights, Norway
-//       </div>
-//     </div>
-//
-//     <div className="mySlides fade" id="image">
-//       <img className="slide_image" src="styles/3.jpg" />
-//       <div className="caption1">
-//         Beautiful Mountains
-//       </div>
-//     </div>
-//
-//     <div className="mySlides fade" id="image">
-//       <img className="slide_image" src="styles/4.jpg" />
-//       <div className="caption1">
-//         The Rain Forest
-//       </div>
-//     </div>
-//
-//     <div className="mySlides fade" id="image">
-//       <img className="slide_image" src="styles/5.jpg" />
-//       <div className="caption1">
-//         Mountains!
-//       </div>
-//     </div>
-//
-//     <button className="w3-button w3-display-left w3-black" onClick={prevDivs}>&#10094;</button>
-//     <button className="w3-button w3-display-right w3-black" onClick={plusDivs}>&#10095;</button>
-//
-//     </div>
-//   </div>
-// )
-//  return element
-// }
