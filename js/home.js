edd808c422eaa2b20ce375fa7b77a0ed4fe16d4a
Jeremy Ward <jeremy2929@twc.com>
2017-05-08 08:21:28 -0500

    Display adjustment to home page

--- js/home.js
+++ js/home.js
@@ -16,9 +16,11 @@ export function Home(){
   function mainPage() {
     const elementMain = (
       <div className="home_page">
-        <article className="my_name_area">
-          <h1 className="my_name">Jeremy Ward</h1>
-        </article>
+        <div className="my_name_wrapper" style={{display: "block"}}>
+          <article className="my_name_area">
+            <p className="my_name">Jeremy Ward</p>
+          </article>
+        </div>
         <section className="intro_skills_area">
           <div className="intro_area" id="IntroArea" style={{display: "inline-block"}}>
             <p className="intro_text" id="introText">I began learning web development at The Iron Yard in late November of 2016.  I have been writing code most of my life but HTML, CSS, and JavaScript was new to me.  This site illustrates the progress of my learning through the 12 week boot camp at The Iron Yard, as well as post-graduation projects and development of my skills.  These four projects chronicle my learning through the weeks from beginning to present.  I have plans to improve and further develop everything seen here, as well as beginning new projects to learn new methods of coding.</p>
